28131cea3ec5c9770099297e35cfdbfac0dbe213
Arniel <aceba1@outlook.com>
2020-09-26 13:12:50 -0400

    Replace 'required' attribute with dynamic button text
    I didn't like the weird red glowing effect

--- client/src/components/BookForm.js
+++ client/src/components/BookForm.js
@@ -5,21 +5,38 @@ export default function NewBookForm(props) {
   const {dispatch} = useContext(BookContext);
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [submit, setSubmit] = useState('Add Book');
 
   const handleSubmit = e => {
     e.preventDefault();
-    dispatch({type: 'ADD_BOOK', book: {title, author}});
-    setTitle('');
-    setAuthor('');
-  }
+    if (title === '' || author === '') {
+      if (title === author)
+        setSubmit('Missing Fields!');
+      else if (title === '')
+        setSubmit('Missing Title!');
+      else
+        setSubmit('Missing Author!');
+    } else {
+      dispatch({type: 'ADD_BOOK', book: {title, author}});
+      setTitle('');
+      setAuthor('');
+      setSubmit('Added!');
+    }
+  };
+
+  // VerifySubmitName
+  const VSN = () => {
+    if (submit !== 'Add Book')
+      setSubmit('Add Book');
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="book title" value={title}
-        onChange={e => setTitle(e.target.value)} required />
-      <input type="text" placeholder="author" value={author}
-        onChange={e => setAuthor(e.target.value)} required />
-      <input type="submit" value="add book" />
+      <input type="text" placeholder="Book Title" value={title}
+        onChange={e => { setTitle(e.target.value); VSN(); }} />
+      <input type="text" placeholder="Author" value={author}
+        onChange={e => { setAuthor(e.target.value); VSN(); }} />
+      <input type="submit" value={submit} />
     </form>
   )
 }
