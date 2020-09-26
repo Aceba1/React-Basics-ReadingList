82eda1989248863b03daa0317648352a5699741d
Arniel <aceba1@outlook.com>
2020-09-26 10:43:24 -0400

    Auth Context Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import NavBarFunc from './components/NavBarFunc'
+import AuthContextProvider from './contexts/AuthContext';
 import ThemeContextProvider from './contexts/ThemeContext';
 function App() {
   return (
     <div className="App">
       <NavBarFunc />
+      <AuthContextProvider>
         <ThemeContextProvider>
         </ThemeContextProvider>
+      </AuthContextProvider>
     </div>
   );
 }
---
+++ client/src/contexts/AuthContext.js
@@ -0,0 +1,15 @@
+import React, {createContext, useState, useEffect} from 'react';
+
+export const AuthContext = createContext()
+
+export default function AuthContextProvider(props) {
+  const [isAuth, setIsAuth] = useState(false);
+  const toggleAuth = () => {
+    setIsAuth(!isAuth);
+  }
+  return (
+    <AuthContext.Provider value={{isAuth, toggleAuth}}>
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
