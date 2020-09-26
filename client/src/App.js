68a3dae0cf1f3a0e40e0b35cc2825be558a9bb3e
Arniel <aceba1@outlook.com>
2020-09-26 10:37:16 -0400

    Clear non-required content

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
-//import logo from './logo.svg';
-import './App.css';
 import NavBarFunc from './components/NavBarFunc'
-import NavBarClass from './components/NavBarClass'
 function App() {
   return (
     <div className="App">
-      <label>Class:</label>
-      <NavBarClass />
-      <hr/>
-      <label>Func:</label>
       <NavBarFunc />
     </div>
   );
